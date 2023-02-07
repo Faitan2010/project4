7a93b755c41d8a97d7915b76b0817dcd14e7d0d1
Faitan2010 <jenia1800@gmail.com>
2023-02-07 22:26:54 +0200

    add progress bar

--- src/js/app.js
+++ src/js/app.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
     });
 
     const headerMenu = document.querySelector('.header-info');
-    const headerOnMobile = document.querySelector('.header-on-mobile')
+    const headerOnMobile = document.querySelector('.header-on-mobile');
+    const html = document.querySelector('.html');
     if (window.innerWidth <= 1024) {
       let prevScrollValue = window.scrollY;
       window.addEventListener('scroll', animateHeader);
@@ -19,13 +20,15 @@ $(document).ready(function(){
 
               if (!headerMenu.classList.contains('header-animate')) {
                   headerMenu.classList.add('header-animate');
-                  headerOnMobile.classList.add('active')
+                  headerOnMobile.classList.add('active');
+                  html.classList.add('active')
               }
           } else {
 
               if (headerMenu.classList.contains('header-animate')) {
                   headerMenu.classList.remove('header-animate');
                   headerOnMobile.classList.remove('active')
+                  html.classList.remove('active')
               }
           }
 
