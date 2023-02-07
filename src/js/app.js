c996a0a5aafe00ecc6224316f124478f786d41e8
Faitan2010 <jenia1800@gmail.com>
2023-02-07 21:08:48 +0200

    add anchor-button

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,74 @@ $(document).ready(function(){
       dots: true,
       arrows: false,
     });
+
+    const headerMenu = document.querySelector('.header-info');
+    const headerOnMobile = document.querySelector('.header-on-mobile')
+    if (window.innerWidth <= 1024) {
+      let prevScrollValue = window.scrollY;
+      window.addEventListener('scroll', animateHeader);
+      function animateHeader() {
+          if (prevScrollValue < window.scrollY) {
+
+              if (!headerMenu.classList.contains('header-animate')) {
+                  headerMenu.classList.add('header-animate');
+                  headerOnMobile.classList.add('active')
+              }
+          } else {
+
+              if (headerMenu.classList.contains('header-animate')) {
+                  headerMenu.classList.remove('header-animate');
+                  headerOnMobile.classList.remove('active')
+              }
+          }
+
+          prevScrollValue = window.scrollY;
+      }
+  }
+
+  const menu = document.querySelector('.menu-icon');
+  const header = document.querySelector('.menu-header');
+
+  menu.addEventListener('click', (e) => {
+    header.classList.toggle('active');
+})
+  const anchorButton = new AnchorButton()
+    anchorButton.init()
   });
+
+
+
+function AnchorButton() {
+    this.anchorButton = document.querySelector('#anchor-button');
+    this.formButton = document.querySelector('#button-with-form');
+
+    const _this = this
+    
+    this.movingToContent = () => {
+        _this.formButton.scrollIntoView({block: "center", behavior: "smooth"})
+    }
+    this.anchorButton.addEventListener('click', _this.movingToContent);
+    this.formButton.addEventListener('click', (e) => {
+      e.preventDefault()
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+  // var hiddenElement = document.getElementById("box") - вторая кнопка;
+  // var btn = document.querySelector('.btn') - якорь;
+  
+  // function handleButtonClick() {
+  //    hiddenElement.scrollIntoView({block: "center", behavior: "smooth"});
+  // }
+  
+  // btn.addEventListener('click', handleButtonClick);
